e5790963ff6743a9ef1036509cb22fd7bac9b480
Anup Kumar <anupbe15062011@gmail.com>
2021-08-03 16:27:38 +0530

    popup

---
+++ action-message/force-app/main/default/lwc/myDialog/myDialog.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class MyDialog extends LightningElement {
+    @api isModalOpen = false; 
+}
--- action-message/force-app/main/default/lwc/myMessage/myMessage.js
+++ action-message/force-app/main/default/lwc/myMessage/myMessage.js
@@ -1,13 +1,29 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class MyMessage extends LightningElement {
     // isShow = false;
-    message = "hello world";
+    message = "successfully save";
     isShow = false;
+    @track isModalOpen = false; 
+    openModal() {
+        this.isModalOpen = true;
+    }
 
+    closeModal() {
+        this.isModalOpen = false;
+    }
+
+    submitDetails() {
+        this.isModalOpen = false;
+        // this.isShow = true;
+    }
+    get onIsSave (){
+        return !this.isModalOpen && !this.isShow;
+    }
     onSave(){
-        this.isShow = !this.isShow;
-        console.log("hello!!  777");
+        this.openModal();
+        // this.isShow = !this.isShow;
+        // console.log("hello!!  777");
 
         // event.preventDefault();
         // const messageEvent = new CustomEvent('message', { detail: this.message });
